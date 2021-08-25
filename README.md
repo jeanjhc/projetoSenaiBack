a2703beef2c64ded7eaaf797e4da312b1e9dd0e2
jeanjhc <henrique_carvalho04@hotmail.com>
2021-08-25 11:27:35 -0300

    aula 25/08

---
+++ README.md
@@ -0,0 +1,2 @@
+# senai_overflow_api
+ 
---
+++ src/models/Post.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes } = require("sequelize");
+
+class Post extends Model {
+
+    static init(connection) {
+        super.init(
+            {
+                title: DataTypes.STRING,
+                description: DataTypes.STRING,
+                image: DataTypes.STRING,
+                gist: DataTypes.STRING
+            },
+            {
+              sequelize: connection,
+            }
+        )
+    }
+
+    static associate(models) {
+
+    }
+}
+
+module.exports = Post;
