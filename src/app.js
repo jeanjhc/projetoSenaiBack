c8112b3a548ffc04766e60ea354856394331e0d0
jeanjhc <henrique_carvalho04@hotmail.com>
2021-08-30 11:29:34 -0300

    login com jwt

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const routes = require("./routes");
 
 const app = express();
 
+app.use(express.json());
+
 app.use(routes);
 
 module.exports = app;
---
+++ src/config/auth.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "2d094d8ff744194f516ebcce2ca39706"
+}
